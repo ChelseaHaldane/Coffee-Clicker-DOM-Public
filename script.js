6e2c35a4640862daf2045f0788424ceb086c24c8
Chelsea <angelofwarcraft@gmail.com>
2022-11-19 08:55:45 -0600

    updated almost thrugh slice 3. need to finish and loo at extra credit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//producers = array that holds producer (singular) objects
 /* eslint-disable no-alert */
 
 /***The line below allows us to access the data from the window object.
@@ -7,10 +8,10 @@ const data = window.data;
 /***Before we can begin manipulating the DOM we need to gain access to two DOM Nodes***/
 // 1. Declare a variable bigCoffee that holds reference to the element with id 'big_coffee'.
 // your code here
-
+const bigCoffee = document.getElementById("big_coffee");
 // 2. Declare a variable producerContainer that holds reference to the element with id 'producer_container'.
 // your code here
-
+const producerContainer = document.getElementById("producer_container");
 /***Don't worry about the specifics of the condition in this if statement for now.
  * Just follow the instructions in it to ensure the application has base functionality.
  * We'll discuss in depth later what process is, but it's not necessary just yet.***/
@@ -22,11 +23,15 @@ if (typeof process === 'undefined') {
   /* 1. Add a 'click' event listener to the bigCoffee element(giant coffee emoji) you referenced above.
    * It should call the clickCoffee function below and be passed the global data object.*/
   // your code here
-
+  bigCoffee.addEventListener('click', clickE => {
+    clickCoffee(data);
+  })
   /* 2. Add a 'click' event listener to the producerContainer(Coffee Producers panel) you referenced above.
    * It should call the buyButtonClick function below and be passed the browser event and global data object.*/
   // your code here
-
+  producerContainer.addEventListener('click', clickP => {
+    buyButtonClick(clickP, data);
+  })
   // You don't need to edit this line of code. It calls the tick function passing in the data object every 1000ms or 1s.
   setInterval(() => tick(data), 1000);
 }
@@ -39,10 +44,15 @@ if (typeof process === 'undefined') {
 
 function updateCoffeeView(coffeeQty) {
   // your code here
+  const coffeeCount = document.getElementById("coffee_counter");
+  coffeeCount.innerText = coffeeQty;
 }
 
 function clickCoffee(data) {
   // your code here
+  data.coffee++;
+  updateCoffeeView(data.coffee);
+  renderProducers(data);
 }
 
 /**************
@@ -51,14 +61,33 @@ function clickCoffee(data) {
 
 function unlockProducers(producers, coffeeCount) {
   // your code here
+  //change to true when coffeeCount >= producer.price/2
+  producers.forEach(producer => {
+    if (coffeeCount >= producer.price / 2) {
+      producer.unlocked = true;
+    }
+  })
 }
 
 function getUnlockedProducers(data) {
   // your code here
+  const producersArray = [];
+  data.producers.forEach(producer => {
+    if (producer.unlocked === true) {
+      producersArray.push(producer)
+    }
+  })
+  return producersArray;
 }
 
 function makeDisplayNameFromId(id) {
   // your code here
+  let tempArray = id.split("_");
+  let newUpperCaseArray = [];
+  tempArray.forEach(word => {
+    newUpperCaseArray.push(word.charAt(0).toUpperCase() + word.slice(1));
+  })
+  return newUpperCaseArray.join(" ");
 }
 
 // You shouldn't need to edit this function-- its tests should pass once you've written makeDisplayNameFromId
@@ -84,10 +113,24 @@ function makeProducerDiv(producer) {
 
 function deleteAllChildNodes(parent) {
   // your code here
+
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+
+
 }
 
 function renderProducers(data) {
   // your code here
+  unlockProducers(data.producers, data.coffee)
+  const producerContainer = document.querySelector('#producer_container');
+  deleteAllChildNodes(producerContainer);
+  data.producers.forEach(producer => {
+    if (producer.unlocked) {
+      producerContainer.appendChild(makeProducerDiv(producer));
+    }
+  })
 }
 
 /**************
@@ -96,26 +139,52 @@ function renderProducers(data) {
 
 function getProducerById(data, producerId) {
   // your code here
+  let producerObject = {}
+  data.producers.forEach(producer => {
+    if (producer.id === producerId) {
+      producerObject = producer;
+    }
+  })
+  return producerObject;
 }
 
 function canAffordProducer(data, producerId) {
   // your code here
+  let producerPrice = getProducerById(data, producerId).price;
+  return data.coffee >= producerPrice;
 }
 
 function updateCPSView(cps) {
   // your code here
+  const cpsIndicator = document.getElementById('cps');
+  cpsIndicator.innerText = cps;
 }
 
 function updatePrice(oldPrice) {
   // your code here
+  let newPrice = Math.floor(oldPrice * 1.25);
+  return newPrice;
 }
 
 function attemptToBuyProducer(data, producerId) {
   // your code here
+  const canAfford = canAffordProducer(data, producerId);
+  if (canAfford) {
+    const producerObject = getProducerById(data, producerId);
+    producerObject.qty++;
+    data.coffee -= producerObject.price;
+    let tempPrice = updatePrice(producerObject.price);
+    producerObject.price = tempPrice;
+    data.totalCPS += producerObject.cps;
+  }
+
+  return canAfford;
 }
 
 function buyButtonClick(event, data) {
   // your code here
+  const targetSlicedId = event.target.id.slice(4);
+  const attempt = attemptToBuyProducer(data, targetSlicedId);
 }
 
 function tick(data) {
@@ -132,7 +201,6 @@ function tick(data) {
 // run the tests in Mocha. More on this later.
 // Don't worry if it's not clear exactly what's going on here.
 if (typeof process !== 'undefined') {
-  console.log('------> process here!!!!!: ', process);
   module.exports = {
     updateCoffeeView,
     clickCoffee,
